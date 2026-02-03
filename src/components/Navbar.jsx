771eb3ca0c76abfe826b7752e4538198a31587a4
sanaullah ansari <sanaullaht.social@gmail.com>
2026-02-03 22:54:27 +0530

    fixed nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         whileTap={{ scale: 0.95 }}
         className=" sm:block bg-blue-600  border border-blue-500/50 text-white px-6 py-2 rounded-full text-[14px] font-bold  transition-all  tracking-widest cursor-pointer"
       >
-        Hire M
+        Hire Me
       </motion.a>
     </nav>
   );
